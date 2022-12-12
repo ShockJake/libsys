a5a441e6ade2a5cc90aa9d7cf686f6c16161fdce
Lysunenko, Stanislav <stanislav.lysunenko@sabre.com>
2022-12-12 12:24:28 +0100

    [feature] Added new endpoints, renamed MainController to WebController, moved html files to template folder, adjusted static files location

---
+++ src/main/java/com/university/libsys/controllers/WebController.java
@@ -0,0 +1,33 @@
+package com.university.libsys.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+
+    @RequestMapping("/libraries")
+    public String librariesPage() {
+        return "pages/library_search";
+    }
+
+    @RequestMapping("/books")
+    public String booksPage() {
+        return "pages/book_search";
+    }
+
+    @RequestMapping("/selections")
+    public String selectionsPage() {
+        return "pages/book_selections";
+    }
+
+    @RequestMapping("/about")
+    public String aboutPage() {
+        return "pages/about";
+    }
+}
