f7e7d77ba671ceab842811da0f212748d7b014ff
Lysunenko, Stanislav <stanislav.lysunenko@sabre.com>
2023-01-03 09:53:08 +0100

    [feature/fix] Refactored UserNotFoundAdvice and AlreadyExistingUserAdvice to one UserExceptionAdvice

--- src/main/java/com/university/libsys/web/exceptions/UserExceptionAdvice.java
+++ src/main/java/com/university/libsys/web/exceptions/UserExceptionAdvice.java
@@ -1,5 +1,6 @@
 package com.university.libsys.web.exceptions;
 
+import com.university.libsys.backend.exceptions.AlreadyExistingUserException;
 import com.university.libsys.backend.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,7 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class UserNotFoundAdvice {
+public class UserExceptionAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(AlreadyExistingUserException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String userAlreadyExistsHandle(AlreadyExistingUserException e) {
+        return e.getMessage();
+    }
 
     @ResponseBody
     @ExceptionHandler(UserNotFoundException.class)
